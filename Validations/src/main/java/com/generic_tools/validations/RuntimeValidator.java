f7ebc1d80b9ba0da0c83f222b08267cc97f6be16
taljmars <taljmars@gmail.com>
2017-09-08 23:35:05 +0300

    Set run-time validator to support external validator

--- Validations/src/main/java/com/generic_tools/validations/RuntimeValidator.java
+++ Validations/src/main/java/com/generic_tools/validations/RuntimeValidator.java
@@ -20,6 +20,7 @@ public class RuntimeValidator {
 	}
 
 	public ValidatorResponse validate(Object obj) {
+		System.out.println("Validate " + obj);
 		Set<ConstraintViolation<Object>> constraints = validator.validate(obj);
 		if (constraints.isEmpty()) {
 			return new ValidatorResponse(ValidatorResponse.Status.SUCCESS, "Validated successfully");
